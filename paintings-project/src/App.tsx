f44912319793e108c1b8571a916e8762f6109983
DanielT-Dev <danieltrusca1@outlook.com>
2026-06-25 22:13:17 +0300

    Add Chakra UI framework and fix dependency issues

--- paintings-project/src/App.tsx
+++ paintings-project/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
-    </>
-  )
+    <Button colorScheme="blue">
+      Hello Chakra UI
+    </Button>
+  );
 }
 
-export default App
+export default App;
